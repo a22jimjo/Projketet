aaa6cd32fadd9accb7b170164feb9195584c42c2
Plaskning <102387003+Plaskning@users.noreply.github.com>
2022-12-02 15:34:47 +0100

    Updated Ghost Behaviour + test animations
    Updated Ghost Behaviour + added test animations

--- Assets/Scripts/EnemyGhostScript.cs
+++ Assets/Scripts/EnemyGhostScript.cs
@@ -8,16 +8,25 @@ public class EnemyGhostScript : MonoBehaviour
     List<Transform> wayPoints = new List<Transform>();
     UnityEngine.AI.NavMeshAgent agent;
     GameObject player;
+    Animator animator;
+
+
     [Tooltip("The minimum distance between the character and it's target to attack")]
     [SerializeField] float attackRange;
     [Tooltip("The area where the enemy can detect the player")]
     [SerializeField] float detectRange;
     [Tooltip("Movement speed of the ghost")]
     [SerializeField] float baseMoveSpeed;
+    [Tooltip("The amount of time the ghost waits after an attack")]
+    [SerializeField] float waitAfterAttack;
 
-    [SerializeField] bool canAttack;
-    private float attackTimer;
-    private float attackCooldown;
+
+    //Movement
+    [SerializeField] float rotationSpeed;
+
+    [SerializeField] bool canAttack = true;
+    [SerializeField] private float attackTimer;
+    [SerializeField] private float attackCooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +34,8 @@ public class EnemyGhostScript : MonoBehaviour
         //setup
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+
         baseMoveSpeed = agent.speed;
     }
 
@@ -34,7 +45,10 @@ public class EnemyGhostScript : MonoBehaviour
         if (Vector3.Distance(agent.destination, player.transform.position) <= detectRange)
         {
             ChaseState();
+            
         }
+
+        //RotateTowards();
     }
 
     void ChaseState()
@@ -48,15 +62,15 @@ public class EnemyGhostScript : MonoBehaviour
         if (Vector3.Distance(agent.transform.position, player.transform.position) < attackRange)
         {
             agent.destination = agent.transform.position;
-            Debug.Log("enemy attacks player");
+            
             if (canAttack)
             {
-                //cattack
+                StartCoroutine(Attack(waitAfterAttack));
             }
         }
 
         //Timer
-        if (attackTimer < attackCooldown)
+        /*if (attackTimer < attackCooldown)
         {
             attackTimer += Time.deltaTime;
         }
@@ -64,26 +78,37 @@ public class EnemyGhostScript : MonoBehaviour
         {
             attackCooldown = attackTimer;
             canAttack = true;
-        }
+        }*/
     }
 
     IEnumerator Attack(float waitTime)
     {
+        animator.SetTrigger("Attack");
+        Debug.Log("enemy attacks player");
         canAttack = false;
         attackTimer = 0;
         //run animation
-
         //Wait until reseting attack
         yield return new WaitForSeconds(waitTime);
         canAttack = true;
 
     }
 
+    void RotateTowards()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
 
-    private void OnDrawGizmos()
+
+        private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, detectRange);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
 
         Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position, Vector3.forward);
