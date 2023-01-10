7e410f47f0c0e617cd36dbab485260a1b8c3f914
a22jimjo <a22jimjo@student.his.se>
2023-01-10 17:17:11 +0100

    Lite golem boss grejer

--- Assets/Scripts/Enemy/GolemSmash.cs
+++ Assets/Scripts/Enemy/GolemSmash.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class GolemSmash : MonoBehaviour
 {
-
-    [SerializeField] private int damage;
-
+    
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             if (other.gameObject.TryGetComponent<PlayerStats>(out PlayerStats playerComponent))
             {
-                playerComponent.TakeDamage(damage);
+                playerComponent.TakeDamage(GetComponentInParent<EnemyStats>().damage);
                 gameObject.SetActive(false);
             }
         }
