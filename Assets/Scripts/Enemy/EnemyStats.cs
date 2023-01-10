0b105c376e16ab05350a9f1cb49a379785fed8ac
a22jimjo <a22jimjo@student.his.se>
2023-01-10 16:15:53 +0100

    Gjorde en grej med damage

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,6 +14,7 @@ public class EnemyStats : MonoBehaviour
     public float health;
     public float maxHealth = 3;
     public float damage = 1;
+    public float defense = 1;
 
 
     [SerializeField] private AudioClip[] HurtClips;
@@ -21,6 +23,8 @@ public class EnemyStats : MonoBehaviour
     [SerializeField] private Healthbar _healthbar;
 
     private AudioSource audioSource;
+    
+    private bool[] damageTriggers = new bool[3];
 
     // Start is called before the first frame update
     void Start()
@@ -31,10 +35,14 @@ public class EnemyStats : MonoBehaviour
         _healthbar.UpdateHealthBar(maxHealth, health);
         audioSource = GetComponent<AudioSource>();
         agent = GetComponent<NavMeshAgent>();
+        for (int i = damageTriggers.Length - 1; i >= 0; i--)
+        {
+            damageTriggers[i] = true;
+        }
     }
     public void TakeDamage(float damageAmount)
     {
-        health -= damageAmount;
+        health -= damageAmount - defense;
         _healthbar.UpdateHealthBar(maxHealth, health);
 
         if (health <= 0)
@@ -43,6 +51,11 @@ public class EnemyStats : MonoBehaviour
         }
         else
         {
+            Debug.Log("Tar skada");
+            if (health < maxHealth * 0.1f && damageTriggers[2]) damageTriggers[2] = false;
+            else if (health < maxHealth * 0.25f && damageTriggers[1]) damageTriggers[1] = false;
+            else if (health < maxHealth * 0.5f && damageTriggers[0]) damageTriggers[0] = false;
+            else return;
             //animator.ResetTrigger("Attack");
             animator.SetTrigger("TakeDamage");
             audioSource.PlayOneShot(HurtClips[Random.Range(0, HurtClips.Length)]);
